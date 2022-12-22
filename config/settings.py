4b194621066bc4f451b8b6a88f63504f11c6473d
Kamran <rakhimovkamran@gmail.com>
2022-12-22 17:30:02 +0500

    | Add Remote DB

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
+
+from django.core.management.utils import get_random_secret_key
 import os
 import sys
 import dj_database_url
