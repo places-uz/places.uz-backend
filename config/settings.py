7eb9b6a2b134fdb5cd73194fbb02f6406d213210
Kamran <rakhimovkamran@gmail.com>
2022-12-22 16:51:33 +0500

    | Add Remote DB

--- config/settings.py
+++ config/settings.py
@@ -94,10 +94,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'defaultdb',
+        'USER': 'doadmin',
+        'PASSWORD': 'AVNS_MlR2TjuTS5nGABIdD3j',
+        'HOST': 'db-postgresql-nyc1-60384-do-user-13146800-0.b.db.ondigitalocean.com',
+        'PORT': '25060',
     }
 }
 # Password validation
